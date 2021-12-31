e03b766fe39e341c7687d593ed39ec70b1c47dc2
PrinceSP <princepasombaran@gmail.com>
2021-12-31 10:08:53 +0800

    delete procfile and modified iindex.js

--- index.js
+++ index.js
@@ -13,23 +13,21 @@ const app = express()
 const PORT = process.env.PORT || 8000
 
 app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended:true}))
 
+//route middleware
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/invoice',invoiceRouter)
 
 app.use(morgan('common'))
 app.use(helmet())
-// app.use((req,res,next)=>{
-//   res.setHeader('Access-Control-Allow-Origin','*')
-//   res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE,OPTIONS')
-//   res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization')
-// })
 
 app.use('/',(req,res)=>{
   res.status(200).send('server is connected. this is / endpoint')
 })
-
+//connnect to MongoDB database with mongoose library
+//app listen to the port for localhost server
 mongoose.connect(process.env.MONGO).then(()=>{
   app.listen(PORT,()=>console.log(`server and database has been connected to port:${PORT}`))
 }).catch(e=>console.log(e))
