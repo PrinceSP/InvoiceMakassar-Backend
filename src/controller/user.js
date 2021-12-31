b61197a528ae6b12d6974769828a561e02190822
PrinceSP <princepasombaran@gmail.com>
2021-12-31 15:45:18 +0800

    modified get user query by find by its userId

--- src/controller/user.js
+++ src/controller/user.js
@@ -43,7 +43,7 @@ exports.getUser = async (req,res)=>{
   const userId = req.query.userId
   const username = req.query.username
   try {
-    const user = userId ? await User.findById(req.params.id) : await User.findOne({username:username})
+    const user = userId ? await User.findById(userId) : await User.findOne({username:username})
     const {password,updatedAt, ...other} = user._doc
     res.status(200).json(other)
   } catch (e) {
