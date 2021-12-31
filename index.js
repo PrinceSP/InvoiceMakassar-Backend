78b3da78214c44c8ff5e967a710b98165b805b00
PrinceSP <princepasombaran@gmail.com>
2021-12-31 09:25:46 +0800

    change port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {authRouter,userRouter,invoiceRouter} = require('./src/router')
 dotenv.config()
 const app = express()
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8000
 
 app.use(bodyparser.json())
 
