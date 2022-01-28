7379211f49b78551d38ebfe32c74bc858de02d17
PrinceSP <princepasombaran@gmail.com>
2022-01-28 12:57:53 +0800

    modified port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const {authRouter,userRouter,invoiceRouter} = require('./src/router')
 dotenv.config()
 const app = express()
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 5000
 
 app.use(bodyparser.json())
 
