c40bdac0053e58317d134f2d88b8f66dc2215967
PrinceSP <princepasombaran@gmail.com>
2022-02-27 15:36:32 +0800

    change json to send

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -37,6 +37,6 @@ exports.login = async (req,res)=>{
     //when there's nothing wrong, then send message
     res.status(200).send({message:'success login',datas:user})
   } catch (e) {
-    res.status(500).json(e)
+    res.status(500).send(e)
   }
 }
